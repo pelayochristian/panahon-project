9f2dd62e5f14ece1f14af83510db3b0266a4123b
christian <pelayochristian.dev@gmail.com>
2020-05-14 21:29:27 +0800

    Add Comment
    Testing the comment.

--- news_service/src/main/java/com/project/panahon/news_service/service/news/NewsService.java
+++ news_service/src/main/java/com/project/panahon/news_service/service/news/NewsService.java
@@ -2,6 +2,11 @@ package com.project.panahon.news_service.service.news;
 
 import java.util.Map;
 
+/**
+ * Interface for the NewsService.
+ *
+ * @author christian
+ */
 public interface NewsService {
     Map<String, Object> newsAPI(String country);
 }
